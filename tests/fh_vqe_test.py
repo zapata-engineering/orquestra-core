14d836d7b32b2ddd0488e2b0d3578b196013885a
ixime <ixchel.meza.ch@gmail.com>
2022-06-14 01:26:45 -0500

    change optimizer and ansatz to converge fh_vqe

--- tests/fh_vqe_test.py
+++ tests/fh_vqe_test.py
@@ -4,13 +4,10 @@
 from functools import partial
 
 import numpy as np
+import pytest
 from orquestra.integrations.qiskit.simulator import QiskitSimulator
-from orquestra.opt.gradients import finite_differences_gradient
 from orquestra.opt.optimizers import ScipyOptimizer
-from orquestra.quantum.estimation import (
-    calculate_exact_expectation_values,
-    estimate_expectation_values_by_averaging,
-)
+from orquestra.quantum.estimation import estimate_expectation_values_by_averaging
 from orquestra.quantum.openfermion import (
     count_qubits,
     get_ground_state,
@@ -56,10 +53,10 @@ class TestVQE:
         jw_hamiltonian = jordan_wigner(operator=hamiltonian)
         backend = QiskitSimulator("aer_simulator_statevector")
 
-        ansatz = HEAQuantumCompilingAnsatz(1, count_qubits(jw_hamiltonian))
+        ansatz = HEAQuantumCompilingAnsatz(2, count_qubits(jw_hamiltonian))
         estimation_method = estimate_expectation_values_by_averaging
 
-        shot_allocation = partial(allocate_shots_proportionally, total_n_shots=10000)
+        shot_allocation = partial(allocate_shots_proportionally, total_n_shots=50000)
         estimation_preprocessors = [
             group_greedily,
             perform_context_selection,
@@ -69,18 +66,63 @@ class TestVQE:
             jw_hamiltonian, ansatz, estimation_preprocessors
         )
 
-        optimizer = ScipyOptimizer(method="L-BFGS-B")
+        optimizer = ScipyOptimizer(method="COBYLA", options={"maxiter": 250})
 
         cost_function = create_cost_function(
-            backend,
-            estimation_task_factory,
-            estimation_method,
-            gradient_function=partial(
-                finite_differences_gradient, finite_diff_step_size=1e-3
-            ),
+            backend, estimation_task_factory, estimation_method
         )
 
-        # TODO: find good initial parameters for this test
-        initial_params = np.random.random(ansatz.number_of_params)
-        _ = optimizer.minimize(cost_function, initial_params)
-        # assert opt_results.opt_value == pytest.approx(exact_energy)
+        initial_params = np.array(
+            [
+                0.94557831,
+                -1.06991166,
+                0.28233565,
+                0.47443967,
+                0.468148,
+                0.5421605,
+                0.29240645,
+                1.48504768,
+                0.48147571,
+                0.36578263,
+                0.45862184,
+                -1.32968834,
+                -0.307463,
+                0.37939154,
+                1.45632776,
+                0.09292365,
+                0.80710492,
+                1.14496005,
+                0.65499076,
+                -0.06335737,
+                0.10678299,
+                0.28446654,
+                0.5538357,
+                0.44774159,
+                0.84030601,
+                1.77205874,
+                0.17598451,
+                0.88785342,
+                -0.5026344,
+                0.72818643,
+                -0.00207843,
+                0.69474356,
+                0.81166782,
+                -0.11638733,
+                -0.24970387,
+                1.02757601,
+                0.24950334,
+                0.95664434,
+                -0.35552715,
+                -0.42096424,
+                1.30391856,
+                0.94936243,
+                0.21281127,
+                -1.36481705,
+                0.37734196,
+                0.0475623,
+                1.1587143,
+                0.9340059,
+            ]
+        )
+        opt_results = optimizer.minimize(cost_function.function, initial_params)
+        assert opt_results.opt_value == pytest.approx(exact_energy, rel=2e-2, abs=2e-2)
